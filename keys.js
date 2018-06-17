58cbde4144bae6aa6eb64ec71778183da0b5476d
marioreg <marioreg@hotmail.com>
2018-06-17 11:57:20 -0500

    Added files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.9i6tLZY1WeIJ0GQX8gAFQk5x4,
+  consumer_secret: process.env.JnpHj5oRX0vhbBoQCaQRTts2qfnXrvLTIhEMF2fwvevoDui15f,
+  access_token_key: process.env.1007112762252722181-SWUXjaP2NvgJ003I0OaOQtV7tKlmME,
+  access_token_secret: process.env.knmLPi3DrzbYVYrqmo5YJAyHUdrpKRyJB60QKSBgStvnu
+};
+
+exports.spotify = {
+  id: process.env.64ef855727e4484a892f3d973f8b3195,
+  secret: process.env.808f77209b1743f9a5e6d13b1b2ead52
+};
