73ad193b5998cb3a0034ac5cbd07e25671077e71
marioreg <marioreg@hotmail.com>
2018-06-24 14:25:48 -0500

    final version completed

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ var omdb = require("omdb");
 var fs = require("fs");
 var keys = require('./keys.js');
 var instructionValue="";
-
+var divider = "\n------------------------------------------------------------\n\n";
 //These are all the instructions the user can enter
 //my-tweets
 //spotify-this-song
@@ -15,153 +15,159 @@ var instructionValue="";
 //twitter user is @MarioRe59364418
 
 //declare variables that store user´s input
+
 var userInstruction = process.argv[2];
 var nodeArgs = process.argv;
 //this loop will get argv from 3 to end and convert them into a string with the "+" sign between the spaces
 for (var i = 3; i<nodeArgs.length; i++){
 instructionValue = instructionValue + "+" + nodeArgs[i];
 }
-//This condition removes the "+" sign from the beggining of the string
-if( instructionValue.charAt(0) === '+' ){
-    instructionValue = instructionValue.slice(1);
-}
+//This removes the "+" sign from the beggining of the string
+instructionValue = instructionValue.slice(1);
 
+start();
+
+function start(){
     switch (userInstruction) {
         case "my-tweets":
             tweets();
             break;
         case "spotify-this-song":
-            spotify();        
+            spotify();
             break;
         case "movie-this":
-            movies();        
+            movies();
             break;
         case "do-what-it-says":
-            doWhatItSays();        
-            break;
-        case "doSomething":
-            doSomething();        
+            doWhatItSays();
             break;
         default:
             break;
     }
-        
+}
+
 
 function movies() {
-
-
     if (instructionValue!=""){
-
         var queryUrl = "http://www.omdbapi.com/?t=" + instructionValue + "&y=&plot=short&apikey=trilogy";
-               
         request(queryUrl, function(error, response, body){
-            if (!error && response.statusCode === 200){        
-            console.log("Title: "+ JSON.parse(body).Title);
-            console.log("Release year: " + JSON.parse(body).Year);
-            console.log("IMDB rating: "+ JSON.parse(body).Ratings[0].Value);
-            console.log("Rotten tomatoes rating: "+ JSON.parse(body).Ratings[1].Value);
-            console.log("Country: "+ JSON.parse(body).Country);
-            console.log("Language: "+ JSON.parse(body).Language);
-            console.log("Plot: "+ JSON.parse(body).Plot);
-            console.log("Actors: "+ JSON.parse(body).Actors);
-            }        
+            if (!error && response.statusCode === 200){
+                var jsonData = JSON.parse(body);
+                var display = [
+                    "Title: " + jsonData.Title,
+                    "Release year: " + jsonData.Year,
+                    "IMDB rating: "+ jsonData.Ratings[0].Value,
+                    "Rotten tomatoes rating: "+ jsonData.Ratings[1].Value,
+                    "Country: "+ jsonData.Country,
+                    "Language: "+ jsonData.Language,
+                    "Plot: "+ jsonData.Plot,
+                    "Actors: "+ jsonData.Actors
+                ].join("\n\n");
+                console.log(display);
+                //call the logTofile function to log result to "log.txt"
+                logToFile(display);
+            }
             });
-
-
     } else {
-
         var queryUrl = "http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=trilogy";
-               
     request(queryUrl, function(error, response, body){
-        if (!error && response.statusCode === 200){        
-        console.log("Title: "+ JSON.parse(body).Title);
-        console.log("Release year: " + JSON.parse(body).Year);
-        console.log("IMDB rating: "+ JSON.parse(body).Ratings[0].Value);
-        console.log("Rotten tomatoes rating: "+ JSON.parse(body).Ratings[1].Value);
-        console.log("Country: "+ JSON.parse(body).Country);
-        console.log("Language: "+ JSON.parse(body).Language);
-        console.log("Plot: "+ JSON.parse(body).Plot);
-        console.log("Actors: "+ JSON.parse(body).Actors);
-        }        
+        if (!error && response.statusCode === 200){
+            var jsonData = JSON.parse(body);
+            var display = [
+                "Title: " + jsonData.Title,
+                "Release year: " + jsonData.Year,
+                "IMDB rating: "+ jsonData.Ratings[0].Value,
+                "Rotten tomatoes rating: "+ jsonData.Ratings[1].Value,
+                "Country: "+ jsonData.Country,
+                "Language: "+ jsonData.Language,
+                "Plot: "+ jsonData.Plot,
+                "Actors: "+ jsonData.Actors
+            ].join("\n\n");
+            console.log(display);
+            logToFile(display);
+        }
         });
     }
-
-    
 }
-
 
 function tweets (){
-    
     var Twitter = require('twitter');
     var client = new Twitter(keys.twitter);
- 
+
     var params = {screen_name: instructionValue, count: 20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {          
+      if (!error) {
        if (tweets.length <=20){
         for (var i=0; i<tweets.length; i++){
-            console.log("Tweet #" + (i+1) + " : " + tweets[i].text + "----------creation date: " + tweets[i].created_at);            
+            var display = [
+                "Tweet #"+ (i+1) + " : " + tweets[i].text + "----------creation date: " + tweets[i].created_at,
+            ].join("\n\n");
+            console.log(display);
+            logToFile(display);
         }
-       } else {
-        for (var i=0; i<20; i++){
-            console.log("Tweet #" + (i+1) + " : " + tweets[i].text + "----------creation date: " + tweets[i].created_at);            
-        }
-       }      
+       }
       }
-    });  
+    });
 }
 
-function spotify(){  
-    
+function spotify(){
+
     if (instructionValue!=""){
         var spotify = new Spotify(keys.spotify);
-        spotify.search({ type: 'track', query: instructionValue, limit: 3 }, function(err, data) {
+        spotify.search({ type: 'track', query: instructionValue, limit: 1 }, function(err, data) {
             if (err) {
               return console.log('Error occurred: ' + err);
-            } 
-                console.log("Artist name: " + data.tracks.items[0].artists[0].name);
-                console.log("Song name: " + data.tracks.items[0].name);
-                console.log("Album name: " + data.tracks.items[0].album.name);
-                console.log("Preview URL: " + data.tracks.items[0].external_urls.spotify);
-                        
-          });        
+            }
+            var display = [
+                "Artist name: " + data.tracks.items[0].artists[0].name,
+                "Song name: " + data.tracks.items[0].name,
+                "Album name: " + data.tracks.items[0].album.name,
+                "Preview URL: " + data.tracks.items[0].external_urls.spotify
+        ].join("\n\n");
+        console.log(display);
+        logToFile(display);
+          });
 
     } else {
 
         var spotify = new Spotify(keys.spotify);
-    spotify.search({ type: 'track', query: "The sign ace of base", limit: 1 }, function(err, data) {
+        spotify.search({ type: 'track', query: "The sign ace of base", limit: 1 }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
-        }                
-            console.log("Artist name: " + data.tracks.items[0].artists[0].name);
-            console.log("Song name: " + data.tracks.items[0].name);
-            console.log("Album name: " + data.tracks.items[0].album.name);
-            console.log("Preview URL: " + data.tracks.items[0].external_urls.spotify);
-      
-        
+        }
+        var display = [
+            "Artist name: " + data.tracks.items[0].artists[0].name,
+            "Song name: " + data.tracks.items[0].name,
+            "Album name: " + data.tracks.items[0].album.name,
+            "Preview URL: " + data.tracks.items[0].external_urls.spotify
+    ].join("\n\n");
+    console.log(display);
+    logToFile(display);
       });
     }
-    
+
 }
 
 function doWhatItSays (){
-   
+
     fs.readFile("random.txt", "utf8", function(error, data){
 
         if (error){
             return console.log(error);
         }
-        
-        var randomText = data.split(",");        
-        for (var i=0; i<randomText.length; i++){
-            console.log("Element "+ i + ": " +randomText[i]);            
-        }
- 
-        
-        
+        var randomText = data.split(",");
+        userInstruction = randomText[0];
+        instructionValue = randomText[1];
 
+        console.log(userInstruction);
+        console.log(instructionValue);
+        start();
         });
-        
-   
+}
 
+
+function logToFile(varToLog){
+    fs.appendFile("log.txt", varToLog + divider, function(err) {
+        if (err) throw err;
+      });
 }
